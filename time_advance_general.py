6cbc71f57095e8e8f22f56c59c24930569a8dd40
jungerstein <BeatlesFan1987@gmail.com>
2016-06-12 22:09:26 +0800

    Draw the evolution.

--- time_advance_general.py
+++ time_advance_general.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+
 def time_advance_profile_with_constant_dt(advancing_func, array_init, dx, dt, n_step_max, *args):
     the_profile = [array_init]
     the_solution = array_init.copy()
@@ -5,3 +9,14 @@ def time_advance_profile_with_constant_dt(advancing_func, array_init, dx, dt, n_
         the_solution = advancing_func(the_solution, dx, dt, *args)
         the_profile.append(the_solution, dx, dt)
     return the_profile
+
+
+def plot_profile(profile, dx, dt_list, filename, title, cmin=-1.0, cmax=1.0):
+    nx = len(profile[0])
+    x = np.linspace(0, (nx + 1) * dx, nx + 1)
+    pyplot.pcolorfast(x, dt_list, profile)
+    pyplot.clim(cmin, cmax)
+    pyplot.set_cmap('PrGn')
+    pyplot.title(title)
+    pyplot.colorbar()
+    pyplot.savefig(filename)
